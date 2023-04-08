8a1da8c854d66d4a91cded574f0fc87a7345d17b
MIHAICosmin1 <mihai.cosmin955@gmail.com>
2023-04-08 13:55:46 +0300

    Initial Project Add

---
+++ src/main/java/cosmin/springframework/cosminpetclinic/CosminPetClinicApplication.java
@@ -0,0 +1,13 @@
+package cosmin.springframework.cosminpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosminPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosminPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cosmin/springframework/cosminpetclinic/CosminPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package cosmin.springframework.cosminpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosminPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
